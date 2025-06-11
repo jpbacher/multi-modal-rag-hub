a36f079f772890a19149a4fda980536a0dec290e
jpbacher <bacherjp@gmail.com>
2025-06-11 11:28:31 -0400

    added project_name variable

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -2,3 +2,9 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "project_name" {
+  description = "A short, lowercase name for this project"
+  type        = string
+  default     = "multi-modal-rag-hub"
+}
