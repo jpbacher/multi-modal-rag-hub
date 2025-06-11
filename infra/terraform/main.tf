68a94cd5a28f07d6560980fda7aacb88daa2dbc3
jpbacher <bacherjp@gmail.com>
2025-06-11 11:33:33 -0400

    added main file

---
+++ infra/terraform/main.tf
@@ -0,0 +1,13 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "documents" {
+  bucket = "${var.project_name}-docs-${random_id.suffix.hex}"
+  acl    = "private"
+
+  tags = {
+    Name        = "Document Bucket"
+    Environment = "dev"
+  }
+}
