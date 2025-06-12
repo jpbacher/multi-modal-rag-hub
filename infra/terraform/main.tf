f79dc29ec672804ef723c68474c2a4c699f8d874
jpbacher <bacherjp@gmail.com>
2025-06-12 13:44:16 -0400

    added iam_role resource

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -10,3 +10,16 @@ resource "aws_s3_bucket" "documents" {
     Environment = "dev"
   }
 }
+
+resource "aws_iam_role" "ingest_lambda_role" {
+  name = "${var.project_name}-ingest-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
