989063507a79a3e9bcc2f3f6f108d502f61ecb1f
jpbacher <bacherjp@gmail.com>
2025-06-23 14:50:52 -0400

    added file

---
+++ handler.py
@@ -0,0 +1,12 @@
+def lambda_handler(event, context):
+    """
+    Entry point for AWS Lambda. 
+    ‘event’ will contain your S3 notification payload,
+    and ‘context’ has metadata about the invocation.
+    """
+    # logic to process event will go here
+    print("Received event:", event)
+    return {
+        "statusCode": 200,
+        "body": "Ingest process started successfully."
+    }
