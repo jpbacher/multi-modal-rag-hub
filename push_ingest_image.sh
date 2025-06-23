78878a7c8bbe50b3fc20140cd0cfd14b1c3b334e
jpbacher <bacherjp@gmail.com>
2025-06-23 16:34:27 -0400

    added comments

--- push_ingest_image.sh
+++ push_ingest_image.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This script builds a Docker image for the ingest service and pushes it to AWS ECR.
+# Make sure it is executable: chmod +x push_ingest_image.sh
+
 # Exit on any error
 set -e 
 
