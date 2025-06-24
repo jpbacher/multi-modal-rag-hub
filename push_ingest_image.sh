58fb9b00c5d88e76431a87306b0d7c56c24e41e8
jpbacher <bacherjp@gmail.com>
2025-06-24 16:22:48 -0400

    used Git SHA for tagging

--- push_ingest_image.sh
+++ push_ingest_image.sh
@@ -11,19 +11,18 @@ set -o allexport
 source .env
 set +o allexport
 
+# Get short Git SHA for tagging
+TAG=$(git rev-parse --short HEAD)
+export IMAGE_URI=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME-ingest:$TAG
+
 # Login to ECR
 aws ecr get-login-password --region $AWS_REGION | \
   docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-# Disables the BuildKit builder (which always builds in OCI mode by default, even without buildx)
-export DOCKER_BUILDKIT=0
+# Disable BuildKit for compatibility with older Docker versions
+export DOCKER_BUILDKIT=0  
+# Build and push image with unique tag
+docker build --no-cache -t $IMAGE_URI .
+docker push $IMAGE_URI
 
-# Single-platform build, and ensures a fresh image with a new digest.
-docker build --no-cache \
-  -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME-ingest:latest .
-
-# Push to ECR
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME-ingest:latest
-
-
-echo "Image pushed to ECR: $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME-ingest:latest"
+echo "Image pushed to ECR: $IMAGE_URI"
