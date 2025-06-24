62cb9fe6a0d2cc2c84198efef788718a302c97b0
jpbacher <bacherjp@gmail.com>
2025-06-24 08:58:04 -0400

    added lambda permission, bucket notification resources

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -60,3 +60,22 @@ resource "aws_ecr_repository" "ingest_repo" {
     Project     = var.project_name
   }
 }
+
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ingest.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.documents.arn
+}
+
+resource "aws_s3_bucket_notification" "on_upload" {
+  bucket = aws_s3_bucket.documents.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.ingest.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
